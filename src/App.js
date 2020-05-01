5d12599d23271129af0400e37d955d9905e4557c
KuffDeSchmull <kuffer.s7@hotmail.com>
2020-05-01 17:13:44 +0200

    added electron

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+
+        <div className = "App">
+                <div className = "sidebar">
+            <div className="sidebarInnerShell"> 
+            </div> 
+            <div className="pullyShell">
+                <div className = "pullyContainer"> 
+                    <div className="pully">< /div>
+                </div>
+            </div> 
+        </div> 
+            <header className = "App-header" >
+        {/*<img src = {logo} className = "App-logo" alt = "logo" />*/}
+            </header> 
+        </div>
+    );
 }
 
 export default App;
